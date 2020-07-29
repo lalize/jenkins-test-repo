5cfba58d5aaf4a975bae3a41c72aa9b96c1c80b2
lalize <printf@kakao.com>
2020-07-29 20:18:32 +0900

    initial commit

---
+++ src/main/java/co/mintcho/lalize/LalizeApplication.java
@@ -0,0 +1,13 @@
+package co.mintcho.lalize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/mintcho/lalize/LalizeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mintcho.lalize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
