1b6ff1684aec6847568f2e6ceacda66417eb824d
lalize <printf@kakao.com>
2020-07-29 20:22:32 +0900

    feat: add greet controller

---
+++ src/main/java/co/mintcho/lalize/controller/GreetController.java
@@ -0,0 +1,13 @@
+package co.mintcho.lalize.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+    @GetMapping("/greet")
+    public ResponseEntity<String> greet() {
+        return ResponseEntity.ok("hello");
+    }
+}
